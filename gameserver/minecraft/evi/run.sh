c2018ba14870647735083fc251d79d3ae83e427a
zekroTJA <riho@live.de>
2020-08-15 15:14:12 +0200

    update evi image

--- gameserver/minecraft/evi/run.sh
+++ gameserver/minecraft/evi/run.sh
@@ -1,18 +1,10 @@
 #!/bin/sh
 
-TMP_CONF_LOC="./_config"
-FIN_CONF_LOC="./config"
-
 TMP_PROP_LOC="./_server.properties"
 FIN_PROP_LOC="./server.properties"
 
 set -x
 set -e
-
-if [ -d $TMP_CONF_LOC ]; then
-    echo "Synchronizing pre-deployed configurations..."
-    mv $TMP_CONF_LOC/* $FIN_CONF_LOC/
-fi
 
 if [ -f $TMP_PROP_LOC ] &&\
    [ -f $FIN_PROP_LOC ] &&\ 
