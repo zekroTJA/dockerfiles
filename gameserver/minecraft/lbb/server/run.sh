252c3b81034f0f043c305748493ed1344ba9c5f1
zekroTJA <riho@live.de>
2023-01-14 17:05:02 +0000

    update startup script

--- gameserver/minecraft/lbb/server/run.sh
+++ gameserver/minecraft/lbb/server/run.sh
@@ -3,4 +3,4 @@
 # Add custom JVM arguments to the user_jvm_args.txt
 # Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
 #  pass them to this script directly
-java @libraries/net/minecraftforge/forge/1.19.2-43.2.0/unix_args.txt nogui "$@"
+java nogui -Xmx$XMX -Xms$XMS @libraries/net/minecraftforge/forge/1.19.2-43.2.0/unix_args.txt "$@"
